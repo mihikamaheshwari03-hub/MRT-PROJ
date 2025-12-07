f7a2e2b7898b313d67fcf2ab43168f97ef526b0e
ramsinghal08 <singhalram44@gmail.com>
2025-12-07 21:44:57 +0530

    updated see function

--- src/swarm_logic/swarm_logic/bot.py
+++ src/swarm_logic/swarm_logic/bot.py
@@ -37,41 +37,81 @@ class bot(Node):
         msg.y = y
         msg.status = status
         self.send_map.publish(msg)
-    def see(self):
-        points = []
-        for x in range(1,self.LOS+1):
-            v = self.get_map_info(self.coord[0]+x,self.coord[1])
-            self.map.setValue(self.coord[0]+x,self.coord[1],v)
-            self.sendmap(x,self.coord[1],v)
-            points.append((self.coord[0]+x,self.coord[1]))
-            if v == 1:
-                break
-        for x in range(1,self.LOS+1):
-            v = self.get_map_info(self.coord[0]-x,self.coord[1])
-            self.map.setValue(self.coord[0]-x,self.coord[1],v)
-            self.sendmap(x,self.coord[1],v)
-            points.append((self.coord[0]-x,self.coord[1]))
-            if v == 1:
-                break
 
-        for y in range(1,self.LOS+1):
-            v = self.get_map_info(self.coord[0],self.coord[1]+y)
-            self.map.setValue(self.coord[0],self.coord[1]+y,v)
-            self.sendmap(self.coord[0],y,v)
-            points.append((self.coord[0],self.coord[1]+y))
-            if v == 1:
+    def see(self):
+        for x in range(self.x - self.LOS + 1, self.x + self.LOS):
+            for y in range(self.y - self.LOS + 1, self.y + self.LOS):
+                
+                if (x == self.x and y == self.y):
+                    continue
+    
+                dx = x - self.x
+                dy = y - self.y 
+    
+                is_blocked = False
+                
+                if abs(dx) > 0 and abs(dy) > 0:
+                    sign_x = 1 if dx > 0 else -1
+                    sign_y = 1 if dy > 0 else -1
+                    
+                    point_1 = (x - sign_x, y)
+                    point_2 = (x, y - sign_y)
+                    middle_point = (x - sign_x, y - sign_y)
+                    
+                    if self.get_map_info.get(point_1) == 1 and self.get_map_info.get(point_2) == 1:
+                        is_blocked = True
+    
+    
+                else:
+                    prev_x = x - (1 if dx > 0 else -1) if dx != 0 else x
+                    prev_y = y - (1 if dy > 0 else -1) if dy != 0 else y
+                    
+                    if (prev_x, prev_y) != (self.x, self.y) and self.get_map_info.get((prev_x, prev_y)) == 1:
+                        is_blocked = True
+    
+                if not is_blocked:
+                    self.self.get_map_info.setValue(x, y, self.get_map_info[(x, y)])
+    
+    
+        #now for outer box
+        is_blocked = 0
+        for dx in range(1,self.LOS):
+            if self.get_self.get_map_info_info((self.x + dx, self.y)) == 1:
+                is_blocked = 1
                 break
-        for y in range(1,self.LOS+1):
-            v = self.get_map_info(self.coord[0],self.coord[1]-y)
-            self.map.setValue(self.coord[0],self.coord[1]-y,v)
-            self.sendmap(self.coord[0],y,v)
-            points.append((self.coord[0],self.coord[1]-y))
-            if v == 1:
+        if not is_blocked:
+            self.self.get_map_info.setValue(self.x + self.LOS, self.y, self.get_map_info[(self.x + self.LOS, self.y)])
+        
+    
+        is_blocked = 0
+        for dy in range(1,self.LOS):
+            if self.self.get_map_info.get((self.x , self.y + dy)) == 1:
+                is_blocked = 1
                 break
-        self.parent.map.grid[self.coord]= 2
+        if not is_blocked:
+            self.self.get_map_info.setValue(self.x, self.y + self.LOS, self.get_map_info[(self.x, self.y + self.LOS)])
+    
+        is_blocked = 0
+        for dx in range(1,self.LOS):
+            if self.self.get_map_info.get((self.x - dx, self.y)) == 1:
+                is_blocked = 1
+                break
+        if not is_blocked:
+            self.self.get_map_info.setValue(self.x - self.LOS, self.y, self.get_map_info[(self.x - self.LOS, self.y)])
+        
+    
+        is_blocked = 0
+        for dy in range(1,self.LOS):
+            if self.self.get_map_info.get((self.x , self.y - dy)) == 1:
+                is_blocked = 1
+                break
+        if not is_blocked:
+            self.self.get_map_info.setValue(self.x ,self.y - self.LOS, self.get_map_info[(self.x, self.y - self.LOS)])
+    
+        self.parent.self.get_map_info.grid[self.coord]= 2
         self.parent.update_data()
-        self.parent.map.update_frontiers()
-        self.parent.loadmap()
+        self.parent.self.get_map_info.update_frontiers()
+    
     def move(self,coord: tuple):
         if coord == (self.coord[0]+1,self.coord[1]) or (self.coord[0]-1,self.coord[1]) or (self.coord[0],self.coord[1]+1) or (self.coord[0],self.coord[1]-1):
             self.coord = coord
